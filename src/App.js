f2a4e291412421ba4d495359ce4f59e8ef4961a1
Vikash Kumar Gupta <vikashkg12@gmail.com>
2025-01-13 20:03:18 +0530

    Fix line ending issues

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Home from './components/Home';
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import WorkSpace from './components/WorkSpace';
+
+function App() {
+  return (
+    <>
+      <Toaster position='top-right'></Toaster>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path="/work/:roomId/:userName" element={<WorkSpace/>}/>
+          {/* <Route path='/editor/:roomId/:userName' element={<Editor></Editor>}></Route> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
